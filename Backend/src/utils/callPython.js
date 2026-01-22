91572954f1e96e3230b20101ee756949a273e631
Omkar Dattatraya Babar <omkarbabar2508@gmail.com>
2026-01-22 22:24:01 +0530

    Fix scheduler API URL env usage

--- Backend/src/utils/callPython.js
+++ Backend/src/utils/callPython.js
@@ -2,13 +2,14 @@ import axios from "axios";
 
 export async function callPythonScheduler(payload) {
   try {
-    const response = await axios.post("${process.env.PYTHON_API_URL}/generate", payload, {
-      timeout: 20000,
-    });
-
-
+    const response = await axios.post(
+      `${process.env.PYTHON_API_URL}/generate`,
+      payload,
+      { timeout: 20000 }
+    );
 
     return response.data;
+
   } catch (err) {
     console.error("Scheduler API Error:", err.message);
     throw new Error("Scheduler API Not Responding");
